18f55501c973aaba40b3b0b3d92cb3452c83a305
tsukuyomiii <73029262+tsukuyomiii@users.noreply.github.com>
2021-06-21 08:47:38 +0530

    Added error handling and prettified JSON response (#1)
    * Prettified JSON Response
    * Adds `InvalidJson` Exception

--- json_server/main.py
+++ json_server/main.py
@@ -1,11 +1,14 @@
 import click
 import json
-from flask import Flask
+from flask import Flask,make_response
 from flask.json import jsonify
 from flask_cors import CORS
-
 app = Flask(__name__)
 CORS(app)
+
+
+class InvalidJson(Exception):
+    pass
 
 @click.command()
 @click.argument('filename', type=click.Path(exists=True))
@@ -14,8 +17,13 @@ def cli_command(filename, port):
 
     @app.route("/", methods=["GET"])
     def index():
-        with open(click.format_filename(filename)) as file:
-            contents = json.loads(file.read())
-        return jsonify(contents)
+        with open(click.format_filename(filename)) as f:
+            try:
+                contents = json.loads(f.read())
+            except json.JSONDecodeError as e:
+                raise InvalidJson("Enter a proper JSON file")
+            response = make_response((json.dumps(contents,indent=4)))
+            response.content_type = "application/json"
+            return response
 
     app.run(debug=True, port=port)
